a84b8e0be6aa2fdecf095a8a766b6554c42e0057
Bryan Rodriguez Abarca <vryahn@MacBook-Pro-de-Bryan.local>
2024-03-23 12:48:16 -0600

    adding wait_for_db for db connections

--- app/app/settings.py
+++ app/app/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for app project.
 
@@ -72,9 +74,16 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
+ 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':os.environ.get('DB_HOST'),
+        'NAME':os.environ.get('DB_NAME'),
+        'USER':os.environ.get('DB_USER'),
+        'PASSWORD':os.environ.get('DB_PASS'),
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
